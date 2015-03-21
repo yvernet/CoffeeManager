34c083a68efb52fdec631dce0921ce5236bed9f2
Yann Vernet <yann.vernet@orange.com>
2015-03-21 21:51:02 +0100

    Fix URI to reach API (for local or dev environment)

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -8,6 +8,6 @@ var cmServices = angular.module('cmServices', ['ngResource']);
 
 cmServices.factory('User', ['$resource',
     function($resource){
-        return $resource('http://localhost:3000/api/users/:userId', {userId:'@id'}, {
+        return $resource('/api/users/:userId', {userId:'@id'}, {
         });
     }]);
